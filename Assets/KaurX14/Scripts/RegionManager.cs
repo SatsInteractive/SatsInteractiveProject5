43f404eefadd2f3526efc5e86dcf9463aa18d360
r66mupall <115570968+r66mupall@users.noreply.github.com>
2024-01-26 22:49:20 +0200

    start

---
+++ Assets/KaurX14/Scripts/RegionManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionManager : MonoBehaviour
+{
+    public GameObject regionUI; // Prefab for the UI to be displayed in the region
+
+    private GameObject uiInstance;
+    private bool playerInsideRegion = false;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Collision with: " + other.gameObject.name);
+            playerInsideRegion = true;
+            // Disable player movement and map
+            // We can change this line of code if player movement is done some other way
+
+            //PlayerController playerController = other.GetComponent<PlayerController>();
+            //playerController.DisableMovement();
+            //MapController.Instance.DisableMap();
+
+            // Instantiate and show region UI
+            uiInstance = Instantiate(regionUI, transform.position, Quaternion.identity);
+            uiInstance.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Exit Collision with: " + other.gameObject.name);
+
+            // Call the ExitRegion method when the player exits the region
+            ExitRegion();
+
+            playerInsideRegion = false;
+        }
+    }
+
+    // Call this method when the player completes the activity and leaves the region
+    public void ExitRegion()
+    {
+            // PlayerController playerController = FindObjectOfType<PlayerController>();
+            // playerController.EnableMovement();
+            Debug.Log("ExitRegion gets called");
+            Destroy(uiInstance);
+            // MapController.Instance.EnableMap();
+    }
+}
